35bc8fd78da6abdbbe93b472be2f702c7e57ea60
Aman Singh <g1aman.singh2575@gmail.com>
2024-08-15 13:31:34 +0530

    Adding Query Parameters and String Validations

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Query
 from enum import Enum
 from pydantic import BaseModel
 
@@ -44,9 +44,9 @@ async def get_food(food_name: FoodEnum):
 fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Jaz"}]
 
 # Query Parameters
-@app.get("/items")
-async def list_items(skip: int = 0, limit: int = 10):
-    return fake_items_db[skip : skip + limit]
+# @app.get("/items")
+# async def list_items(skip: int = 0, limit: int = 10):
+#     return fake_items_db[skip : skip + limit]
 
 # Optional Query Parameters 
 
@@ -100,3 +100,25 @@ async def create_item_with_put(item_id: int, item: Item, q: str | None = None):
     if q:
         result.update({"q": q})
     return result
+
+
+@app.get("/items")
+async def read_items(q: str
+                     | None = Query(
+                         None,
+                         min_length=3,
+                         max_length=10,
+                         title="Sample query string",
+                         description="This is a sample query string",
+                         alias="item-query"
+                     )):   ##q: str = Query("fixedquery",min_length =3, max_length = 10)  ## for required condition Query(...,min_length =3, max_length = 10)
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results.update({"q": q})
+    return results
+
+@app.get("/items_hidden")
+async def hidden_query_route(hidden_query: str | None = Query(None, include_in_schema=False)):
+    if hidden_query:
+        return {"hidden_query": hidden_query}
+    return {"hidden_query": "Not found"}

c233b73d9aadf1a5ce46050721a9cd1684b4de33
Aman Singh <g1aman.singh2575@gmail.com>
2024-08-15 16:10:49 +0530

    Path Parameters and Numeric Validation

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI,Query
+from fastapi import FastAPI,Query,Path
 from enum import Enum
 from pydantic import BaseModel
 
@@ -122,3 +122,17 @@ async def hidden_query_route(hidden_query: str | None = Query(None, include_in_s
     if hidden_query:
         return {"hidden_query": hidden_query}
     return {"hidden_query": "Not found"}
+
+
+@app.get("/items_validation/{item_id}")
+async def read_items_validations(
+    *,
+    item_id: int = Path(..., title="The ID of the item to get", 
+    gt=10, lt=100), 
+    q: str="hekko",
+    size: float = Query(..., gt=0, lt=7.6)
+):
+    results = {"item_id": item_id, "size": size}
+    if q:
+        results.update({'q': q})
+    return results
